0bf232d291677415bf75cf102638edbd8f8f8aea
Damon Schulz <damon.schulz@gmail.com>
2020-07-27 18:17:40 -0500

    created components folder, created css sheet, created title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="row">
+        <h1  className = "col-sm 2 col-md-6 col-lg-12">
+        Flashcards
+        </h1>
+      </div>
+    
   );
 }
+
+
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id:1,
+    question: 'What is 2 + 2?',
+    answer:'4',
+    options:[
+      '5',
+      '4',
+      '3'
+    ]
+  },
+  {
+    id:2,
+    question: 'What is 4 + 4?',
+    answer:'8',
+    options:[
+      '8',
+      '9',
+      '10'
+    ]
+  }
+]
+
 
 export default App;
